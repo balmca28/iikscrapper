d5e001d3a8e40a3b6f397d82bbd30a4e2df2f6f6
baluindia <110904056+baluindia@users.noreply.github.com>
2023-09-28 21:04:04 +0530

    port fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const  mysql = require('mysql');
 const app = require('./app');
 
-const port =  process.env.PORT || 5000;
+const port =  process.env.PORT || 80;
 
 app.listen(port, (req, res)=>{
     console.log(`Server is running on ${port}`);
